a7110f7a02ae549881c25f5b0179457cb274e415
Santi Acosta Verrier <santiav@gmail.com>
2026-04-30 17:35:13 -0300

    ejercicios realizados en clases

--- clase-34/objetos-ejercicios-realizados.js
+++ clase-34/objetos-ejercicios-realizados.js
@@ -5,6 +5,42 @@ let usuarios = [
   { nombre: "Pedro", edad: 25 }
 ];
 
+let estudiantes = [{
+  id: 1,
+  nombre: "Aroa",
+  apellido: "Ramirez",
+  edad: 25,
+  localidad: "Madrid",
+  país: "España",
+  cursando: true
+},
+{
+  id: 2,
+  nombre: "Marcos",
+  apellido: "Oliveros",
+  edad: 52,
+  localidad: "Valencia",
+  país: "España",
+  cursando: true
+},
+{
+  id: 3,
+  nombre: "Sebas",
+  apellido: "Alonso",
+  edad: 40,
+  localidad: "Madrid",
+  país: "España",
+  cursando: true
+},{
+  id: 4,
+  nombre: "Alex",
+  apellido: "Gonzalez",
+  edad: 34,
+  localidad: "Madrid",
+  país: "España",
+  cursando: true
+}]
+
 /* Mostrar nombres de cada usuario */
 for (let usuario of usuarios) {
   console.log(usuario.nombre)
@@ -23,3 +59,36 @@ for (let usuario of usuarios) {
 Mostrar solo los usuarios que:
 tengan 18 años o más
 */
+function chequearEdad(array) { // Toma como parámetro al array que le haya 'enviado'
+  let mensajes = []
+  for (let usuario of array) {
+     if (usuario.edad  >= 18) {
+       mensajes.push(`${usuario.nombre} es mayor de edad`)
+     }
+  } 
+  return mensajes
+  
+}
+chequearEdad(usuarios)
+chequearEdad(estudiantes)
+
+// Ejercicio #3
+/*
+contar cuántos usuarios hay
+contar cuántos son mayores de edad
+*/
+function cuantasPersonas(array) {
+  let totalMayores = 0;
+  for (let persona of array) {
+    if (persona.edad  >= 18) {
+        totalMayores++
+     }
+  }
+  let totalUsuarios = array.length
+  return `Hay ${totalMayores} mayores y son ${totalUsuarios} usuarios`
+}
+cuantasPersonas(usuarios)
+
+
+
+
